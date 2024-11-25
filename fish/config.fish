cf70a9942fb4c3757ea711e548f6ec5485080396
Jayanth Balasubramanian <jayanth.balasubramanian1@gmail.com>
2024-11-25 14:05:25 +0800

    Add various config files

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+alias vim "nvim"
+alias vi "nvim"
+alias oldvim "\vim"
+# alias pr "poetry run"
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/Users/jayanth/.opam/opam-init/init.fish' && source '/Users/jayanth/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
